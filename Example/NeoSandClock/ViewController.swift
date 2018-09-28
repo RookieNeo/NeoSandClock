690bd1a19592974d47e9066ca9082d3fc4bf3c95
Neo <ningjiaguan@meican.com>
2018-09-28 14:51:58 +0800

    dev 2

--- Example/NeoSandClock/ViewController.swift
+++ Example/NeoSandClock/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("dev 1")
+        print("dev 2")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
