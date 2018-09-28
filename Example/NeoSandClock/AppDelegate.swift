61f1f88d4b9e6ad6410bac06d98dcdd0c13cd80b
Neo <ningjiaguan@meican.com>
2018-09-28 14:51:19 +0800

    Master 2

--- Example/NeoSandClock/AppDelegate.swift
+++ Example/NeoSandClock/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         print("master 1")
+        print("master 2")
         // Override point for customization after application launch.
         return true
     }
